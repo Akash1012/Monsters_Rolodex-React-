43406726063eb0404482746d71896a5d7f1cc6e9
Akash1012 <gupta.akash2090@gmail.com>
2019-11-23 19:15:23 +0530

    Updates

---
+++ precache-manifest.36e02d54749a84d207d26090bcea10a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418ceb27b12944cfa8265eb1ff9eec11",
+    "url": "/Monsters_Rolodex-React-/index.html"
+  },
+  {
+    "revision": "54ad9d992fcd4ece7c7c",
+    "url": "/Monsters_Rolodex-React-/static/css/main.ee6a0214.chunk.css"
+  },
+  {
+    "revision": "16a8de3c41b7cbbeb8e7",
+    "url": "/Monsters_Rolodex-React-/static/js/2.3de7d83c.chunk.js"
+  },
+  {
+    "revision": "54ad9d992fcd4ece7c7c",
+    "url": "/Monsters_Rolodex-React-/static/js/main.abbf30fa.chunk.js"
+  },
+  {
+    "revision": "8ef3bafcd46baafbbe77",
+    "url": "/Monsters_Rolodex-React-/static/js/runtime-main.bdc7ca61.js"
+  }
+]);
